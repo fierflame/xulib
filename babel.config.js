09915567d0eaf349a2353a8337464272cca106cb
Unknown <git@wangchenxu.net>
2019-06-21 22:24:38 +0800

    chore: 添加配置及编译文件

---
+++ babel.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+	"plugins": [
+		"@babel/plugin-transform-exponentiation-operator",
+		"@babel/plugin-transform-async-to-generator",
+		"@babel/plugin-proposal-async-generator-functions",
+		"@babel/plugin-proposal-optional-catch-binding",
+		"@babel/plugin-proposal-object-rest-spread",
+		"@babel/plugin-proposal-class-properties",
+		"@babel/plugin-proposal-do-expressions",
+		"@babel/plugin-proposal-export-default-from",
+		"@babel/plugin-proposal-export-namespace-from",
+		"@babel/plugin-proposal-function-bind",
+		"@babel/plugin-proposal-logical-assignment-operators",
+		"@babel/plugin-proposal-nullish-coalescing-operator",
+		"@babel/plugin-proposal-numeric-separator",
+		"@babel/plugin-proposal-optional-chaining",
+		"@babel/plugin-proposal-partial-application",
+		["@babel/plugin-proposal-pipeline-operator", { "proposal": "minimal" }],
+		"@babel/plugin-proposal-private-methods",
+		"@babel/plugin-proposal-throw-expressions",
+		"@babel/plugin-transform-typescript",
+	]
+}
